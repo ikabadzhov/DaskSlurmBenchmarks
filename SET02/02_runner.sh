d3b65f3ec9a92390e9bc2e9cfc50a27764ab0263
Ivan Kabadzhov <kabadzhovivan@gmail.com>
2021-11-01 15:58:39 +0100

    Reports in New set

---
+++ SET02/02_runner.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+unlink ~/distrdf_timestamps.out  # remove the old logs (if any)
+rm *.out
+killall python3
+
+while getopts d: flag
+do
+  case "${flag}" in
+    d) dirn=${OPTARG};;
+  esac
+done
+
+mkdir "${dirn}"
+
+for Nodes in 64 32
+do
+  for p in 2048 4096 8192
+  do
+    CMD="python3 01_distrdf.py -c 32 -N ${Nodes} -f 1000 -p ${p}"
+    printf "\n\n${CMD}\n" >> ~/distrdf_timestamps.out
+    eval "$CMD"
+    mkdir "${dirn}/log_p${p}_N${Nodes}"
+    mv *.out "${dirn}/log_p${p}_N${Nodes}"  # move the newly produced slurm logs
+    mv *.html "${dirn}/log_p${p}_N${Nodes}"
+    echo "$CMD was done"
+    killall python3
+  done
+done
+
+mv ~/distrdf_timestamps.out ./${dirn}
+
